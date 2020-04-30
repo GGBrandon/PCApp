dbf3014bb94c12e3a9af529c9814471a884994fa
drodriguez58 <rodriguez_derek2@yahoo.com>
2020-04-29 21:22:09 -0700

    initial setup

--- App.js
+++ App.js
@@ -4,58 +4,17 @@ import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
 // import the different screens
-import LoadingScreen from './src/screens/LoadingScreen';
-
-
-const RootSwitch = createSwitchNavigator(
-  {
-    LoadingScreen: LoadingScreen,
-  
-  },
-  {
-    initialRouteName: 'LoadingScreen'
-  }
-);
-
-const AuthStack = createStackNavigator(
-  {
-    //LoadingScreen: LoadingScreen,
-    SignUpScreen: SignUpScreen,
-   
-  },
-  {
-    initialRouteName: 'LoginScreen',
-  }
-);
-const RootStack = createStackNavigator(
-  {
-    BookListScreen: BookListScreen,
-    //AuthFlow: {screen: RootSwitch},
-    BookDetailsScreen: BookDetailsScreen,
-    EditBookScreen: EditBookScreen,
-    //BookHistoryScreen: {screen: BookHistoryScreen}
-  },
-  {
-    initialRouteName: 'BookListScreen',
-    navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#777777'
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-      }
-    }
-  }
-);
+//import LoadingScreen from './src/screens/LoadingScreen';
+import Start from './src/screens/Start';
 
 const SwitchNavigator = createSwitchNavigator(
   {
-    LoadingScreen: LoadingScreen,
-    Auth: AuthStack,
-    Root: RootStack
+    Start: Start,
+    //Auth: AuthStack,
+    //Root: RootStack
   },
   {
-    initialRouteName: 'LoadingScreen',
+    initialRouteName: 'Start',
   }
 );
 
--- src/screens/Start.js
+++ src/screens/Start.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
-import { firebase } from '@react-native-firebase/auth';
+//import { firebase } from '@react-native-firebase/auth';
 
-export default class LoadingScreen extends React.Component {
+export default class Start extends React.Component {
     static navigationOptions = {
         title: 'Loading',
         headerLeft: () => null
